26f4c3bccb622ef71a4693b5a9a5e3434c817c70
Matthieu Couronne <m@thieu.dev>
2021-08-18 13:17:34 +0200

    more logs

--- lib/localio/processors/google_drive_processor.rb
+++ lib/localio/processors/google_drive_processor.rb
@@ -90,7 +90,7 @@ class GoogleDriveProcessor
 
     puts 'here 1'
 
-    puts "length() method form : #{matching_spreadsheets[0].worksheets}"
+    puts "length() method form : #{matching_spreadsheets[0].worksheets_feed_url}"
 
     worksheet = matching_spreadsheets[0].worksheet_by_title("Backend")
 
