45a5964a4afe074c313167d5f99d4e516453dc90
Matthieu Couronne <m@thieu.dev>
2021-08-30 09:57:47 +0200

    add log

--- lib/localio/writers/po_writer.rb
+++ lib/localio/writers/po_writer.rb
@@ -9,20 +9,30 @@ class PoWriter
     resource_file = options[:resource_file].nil? ? "default" : options[:resource_file]
 
     languages.keys.each do |lang|
+      puts 'here at 1'
       output_path = File.join(path, "#{lang}/LC_MESSAGES/")
+      puts 'here at 2'
       file_name = "#{resource_file}.po"
+      puts 'here at 3'
 
       # We have now to iterate all the terms for the current language, extract them, and store them into a new array
 
       segments = SegmentsListHolder.new lang
+      puts 'here at 4'
       terms.each do |term|
+        puts 'there at 1'
         key = Formatter.format(term.keyword, formatter, method(:po_key_formatter))
+        puts 'there at 2'
         translation = term.values[lang].gsub(/\n/, '\n')
+        puts 'there at 3'
         segment = Segment.new(key, translation, lang)
+        puts 'there at 4'
         segment.key = nil if term.is_comment?
+        puts 'there at 5'
         segments.segments << segment
+        puts 'there at 6'
       end
-
+      puts 'here at 5'
       TemplateHandler.process_template 'po_localizable.erb', output_path, file_name, segments
       puts " > #{lang.yellow}"
     end
